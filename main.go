78c716823b1a67a59a11f5597b8188733d4323b6
Alban Montaigu <alban.montaigu@gmail.com>
2018-10-15 22:30:57 +0200

    Big big rework to fix issues (all broken now for sure :):):):))

--- main.go
+++ main.go
@@ -1,110 +1,160 @@
 package main
 
+//
+// Dependencies
+//
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"strings"
-  "strconv"
-
-	"github.com/scaleway/scaleway-cli/pkg/api"
+    "encoding/json"
+    "fmt"
+    "os"
+    "strings"
+    "strconv"
+    "github.com/scaleway/go-scaleway"
 )
 
+//
+// Global variables
+//
+var (
+    scwApi ScalewayAPI
+)
+
+//
+// Main function
+//
 func main() {
-	orgToken := os.Getenv("SCALEWAY_ORGANIZATION")
-	token := os.Getenv("SCALEWAY_TOKEN")
 
-	if strings.TrimSpace(orgToken) == "" || strings.TrimSpace(token) == "" {
-		panic("required environmental variables are not set")
-	}
+    // Init API
+    initScwApi()
 
+    // Vars
+    var jsonResponse []byte
+    var err error
 
-  if os.Args[1] == "--list" {
-	  dynamicInventory := make(map[string][]string)
-	  getServers(dynamicInventory, token, orgToken)
+    // Process depending the flags
+    switch os.Args[1] {
+    
+    // Get server list
+    default:
+    case "--list":
+        jsonResponse, err = json.Marshal(getServers())
+    
+    // Get server details
+    case "--host":
+        if os.Args[2] == "" {
+            panic("Hostname is required (--host <hostname>)")
+        }
+        jsonResponse, err = json.Marshal(getServer(os.Args[2]))
+    }
 
-	  body, err := json.Marshal(dynamicInventory)
-	  if err != nil {
-		  panic("failed to marshal the dynamic inventory")
-	  }
+        
+    // Cherck result and displays it if any
+    if err != nil {
+        panic("Failed to marshal the dynamic inventory")
+    }
+    fmt.Println(string(jsonResponse))
 
-	  fmt.Println(string(body))
-  } else if os.Args[1] == "--host" {
-	  dynamicInventory := make(map[string]string)
-	  getServersDetails(dynamicInventory, token, orgToken, os.Args[2])
-
-    jsonString, err := json.Marshal(dynamicInventory)
-	  if err != nil {
-		  panic("failed to marshal the dynamic inventory")
-	  }
-
-    fmt.Println(string(jsonString))
-  }
 }
 
-func getServers(dict map[string][]string, token, orgToken string) {
-	disabledLoggerFunc := func(a *api.ScalewayAPI) {
-		a.Logger = api.NewDisableLogger()
-	}
 
-	api, err := api.NewScalewayAPI(orgToken, token, "Scaleway Dynamic Inventory",
-		"", disabledLoggerFunc)
-	if err != nil {
-		panic(fmt.Sprintf("failed to create API instance: %s", err))
-	}
+//
+// Initialize the common scaleway API object
+//
+func initScwApi () *ScalewayAPI {
 
-	servers, err := api.GetServers(true, 0)
-	if err != nil {
-		panic(fmt.Sprintf("failed to get servers: %s", err))
-	}
+    // Get and control scaleway tokens
+    scwOrga := strings.TrimSpace(os.Getenv("SCALEWAY_ORGANIZATION"))
+    if strings.TrimSpace(scwOrga) == ""  {
+        panic("Required SCALEWAY_ORGANIZATION env var is not set")
+    }
+    scwToken := strings.TrimSpace(os.Getenv("SCALEWAY_TOKEN"))
+    if strings.TrimSpace(scwToken) == "" {
+        panic("Required SCALEWAY_TOKEN env var is not set")
+    }
 
-	for _, server := range *servers {
-		for _, tag := range server.Tags {
-			if _, ok := dict[tag]; !ok {
-				dict[tag] = make([]string, 0)
-			}
-      dict[tag] = append(dict[tag], server.Name)
-		}
-	}
+    // Init api object
+    disabledLoggerFunc := func(a *api.ScalewayAPI) {
+        a.Logger = api.NewDisableLogger()
+    }
+    api, err := api.NewScalewayAPI(scwOrga, scwToken, "Scaleway Dynamic Inventory", "", disabledLoggerFunc)
+    if err != nil {
+        panic(fmt.Sprintf("Failed to create scaleway API instance: %s", err))
+    }
+    scwApi = *api
 }
 
-func getServersDetails(dict map[string]string, token, orgToken string, servername string) {
-	disabledLoggerFunc := func(a *api.ScalewayAPI) {
-		a.Logger = api.NewDisableLogger()
-	}
+//
+// Get servers list (--list flag)
+//
+func getServers() map[string][]string {
 
-	api, err := api.NewScalewayAPI(orgToken, token, "Scaleway Dynamic Inventory",
-		"", disabledLoggerFunc)
-	if err != nil {
-		panic(fmt.Sprintf("failed to create API instance: %s", err))
-	}
+    // API call
+    servers, err := scwApi.GetServers(true, 0)
+    if err != nil {
+        panic(fmt.Sprintf("Failed to get servers: %s", err))
+    }
 
-	servers, err := api.GetServers(true, 0)
-	if err != nil {
-		panic(fmt.Sprintf("failed to get servers: %s", err))
-	}
-  
-  var count int = 0
-  var publicip string = "none"
-	for _, server := range *servers {
-    count++ 
+    // Prepare result
+    result := make(map[string][]string)
+
+    // Build result
+    for _, server := range *servers {
+        for _, tag := range server.Tags {
+            if _, ok := result[tag]; !ok {
+                result[tag] = make([]string, 0)
+            }
+            result[tag] = append(result[tag], server.Name)
+        }
+    }
+    return result
+}
+
+//
+// Get server details (with --host flag)
+//
+func getServer(serverName string) map[string]string {
+
+    // API call
+    server, err := scwApi.GetServer(serverName)
+    if err != nil {
+        panic(fmt.Sprintf("Failed to get server: %s", err))
+    }
+
+    // Prepare result
+    result := make(map[string]string)
+
+    // Build generic stuff
+    result["ansible_python_interpreter"] = "/usr/bin/python3"
+    result["ansible_user"] = "root"
+
+    // Build specific result for proxy0
     if server.Name == "proxy0" {
-      publicip = server.PublicAddress.IP
-      dict["proxy_inet"] = "True"
+        result["proxy_inet"] = "True"
+        result["ansible_ssh_common_args"] = "-o ProxyCommand=\"ssh -W %h:%p -q root@" + server.PublicAddress.IP + " -i ~/.ssh/scaleway.pem\""
     }
-    if server.Name == servername {
-      if server.PublicAddress.IP != "" {
-        dict["ansible_host"] = server.PublicAddress.IP
-      } else {
-        dict["ansible_host"] = server.PrivateIP
-      }
-      dict["ansible_user"] = "root"
-      var vpn_ip string = "192.168.66." + strconv.Itoa(count)
-      dict["vpn_ip"] = vpn_ip
-		}
-    if ! strings.Contains(servername, "proxy0") {
-      dict["ansible_ssh_common_args"] = "-o ProxyCommand=\"ssh -W %h:%p -q root@" + publicip + " -i ~/.ssh/scaleway.pem\""
+
+    // Build ansible hosts and takes care about public / private ip
+    if server.PublicAddress.IP != "" {
+        result["ansible_host"] = server.PublicAddress.IP
+    } else {
+        result["ansible_host"] = server.PrivateIP
     }
-    dict["ansible_python_interpreter"] = "/usr/bin/python3"
-	}
+
+    // Build the vpn_ip
+    lastDigit := server.Name[0:len(server.Name)-1]
+    if _, err := strconv.Atoi(lastDigit); err == nil {
+        switch {
+            case strings.Contains(server.Name, "proxy"):
+                result["vpn_ip"] = "192.168.66.1" + lastDigit
+                break
+            case strings.Contains(server.Name, "master"):
+                result["vpn_ip"] = "192.168.66.2" + lastDigit
+                break
+            case strings.Contains(server.Name, "worker"):
+                result["vpn_ip"] = "192.168.66.3" + lastDigit
+                break
+        }
+    }
+    return result
+
 }
